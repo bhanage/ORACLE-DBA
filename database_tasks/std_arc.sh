8b18cd4dea0633d3b2f1713e9b2c0622e817d251
sanika bhanage <59289830+bhanage@users.noreply.github.com>
2024-02-15 13:02:00 +0530

    Add files via upload

---
+++ database_tasks/std_arc.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+if [ -z $1 ];then
+	echo "Set ORACLE_SID"
+	exit;
+fi
+PATH=$PATH:$HOME/.local/bin:$HOME/bin
+ORACLE_HOME=/orahome/app/oracle/product/19.3.0/db_1
+ORACLE_BASE=/orahome/app/oraInventory
+PATH=$ORACLE_HOME/bin:$PATH
+export ORACLE_SID=$1
+export PATH ORACLE_HOME  ORACLE_BASE
+
+###Check DB is running
+ps -ef | grep ora_pmon_$ORACLE_SID | grep -v grep
+if [ $? -ne 0 ];then
+	echo "Database is not running os level"
+	exit
+fi
+
+###Check DB is mounted mode
+(
+sqlplus -S / as sysdba<<EOF
+select status from v\$instance;
+EOF
+)>/home/oracle/carrot/my_out1.txt
+cat /home/oracle/carrot/my_out1.txt | grep "MOUNTED" >> /dev/null
+if [ $? -ne 0 ];then
+	echo "Database is not in MOUNTED state"
+	exit
+fi
+###Check archivelog mode is enabled
+(
+sqlplus -S / as sysdba<<EOF
+archive log list;
+EOF
+)>/home/oracle/carrot/my_out2.txt
+cat /home/oracle/carrot/my_out2.txt | grep -o "Enabled" >> /dev/null
+if [ $? -ne 0 ];then
+	echo "Not in archive mode"
+	exit
+fi
+###Generate required sequence number
+(
+sqlplus -S / as sysdba<<EOF
+select max(SEQUENCE#) from v\$log_history;
+EOF
+)>/home/oracle/carrot/my_out3.txt
+last_arch_given=`cat my_out3.txt | tail -n 2 | awk '{print $1}'`
+###Remotely copying all the archivals
+req_arc=`expr $last_arch_given + 1`
+day=`date +%d`
+month=`date +%m`
+year=`date +%Y`
+sid_dir=`echo "$ORACLE_SID" | tr [:lower:] [:upper:]`
+date_dir=${year}_${month}_${day} 
+ssh oracle@192.168.25.167 sh /home/oracle/archivelog.sh $1
+count=`ssh oracle@192.168.25.167 ls -1 $ORACLE_BASE/fast_recovery_area/$sid_dir/archivelog/$date_dir/ | wc -l`
+for ((i=1; i<=$count; i++));do
+	
+	filename=`ssh oracle@192.168.25.167 find $ORACLE_BASE/fast_recovery_area/$sid_dir/archivelog/$date_dir/ -name "o1_mf_1_${req_arc}*"`
+	if [ $? -eq 0 ];then
+       		 scp  oracle@192.168.25.167:$ORACLE_BASE/fast_recovery_area/$sid_dir/archivelog/$date_dir/o1_mf_1_${req_arc}* oracle@192.168.25.161:/home/oracle/carrot
+	fi
+	req_arc=`expr $req_arc + 1`
+	
+done
